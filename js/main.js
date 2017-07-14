1cd0ac0f375035c425fd12e730e1f6e914030902
Sagicary <sagicary@gmail.com>
2017-07-13 21:17:46 -0400

    Segundo menu listo con sidebar y enlaces a web

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
 /* Holiii acá va tu código también */
+/*PROBANDO SIDEBAR JS*/
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+/*FIN SIDEBAR*/
+
+$('#btn-sesion').click(function(){
+	/*PONER VALIDACION DE DATOS*/
+	document.location.href = "menu_app.html";
+});
+
+$('#saldo').click(function(){
+	document.location.href = "saldo.html";
+});
+$('#tarifa').click(function(){
+	document.location.href = "tarifa.html";
+});
+$('#perfil').click(function(){
+	document.location.href = "perfil.html";
+});
+$('#faq').click(function(){
+	document.location.href = "faq.html";
+});
+
